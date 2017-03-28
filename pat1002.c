492fddf1a0598f1544a8c7c27c856deaab73088f
cyang <cy950812@gmail.com>
2017-03-28 12:19:25 +0800

    add 1002

--- pat1002.c
+++ pat1002.c
@@ -6,39 +6,91 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
-void parse(int n,int a[]);
+int parse(int n,char *a);
+void printfNum(int n,char *a);
 
 int main()
 {
-  int n = 32;
-  int a[10] = {0};
-  //int b[10] = {0};
-  //scanf("%d",&n);
-  parse(n,a);
-  for(int i = 0;i<10;i++)
+  char n = ' ';
+  int sum = 0;
+  int idx = 0;
+  int temp = 0;
+  char a[100] = {0};
+  char b[100] = {0};
+  //scanf("%ld",&n);
+  //parse(n,a);
+  n = getchar();
+
+  while(n!='\n')
     {
-      printf(" %d ",a[i]);
+      sum = sum + (n-'0');
+      n = getchar();
     }
+  printf("\nsum :%d\n",sum);
+
+  temp = parse(sum,b);
+  idx = 0;
+  while(b[idx]!='\0')
+    {
+      printf("b[idx]:%d ",b[idx]);
+      idx++;
+    }
+
+  printfNum(temp,b);
+
   return 0;
 }
 
-void parse(int n,int a[])
+int parse(int n,char *a)
 {
-  printf("parse");
-  int i=0;
-  for(;;)
+  int i = 0;
+  while(n!=0)
     {
-      if(n!=0)
-	{
-	  a[i] = n%10;
-	  i++;
-	  n/10;
-	  printf("%d",n);
-	}
-      else
-	{
+      a[i++] = n%10;
+      n = n/10;
+      printf("a[%d]: %d ",i-1,a[i-1]);
+    }
+  a[i] = '\0';
+  return i;
+}
+
+void printfNum(int n,char *a)
+{
+  int i = 0;
+  //while(a[i] != '\0')i++;
+  i = n;
+  printf("\n i: %d\n",i);
+  while(--i>=0)
+    {
+      switch(a[i])
+      {
+	case 0 :
+	  printf("ling");break;
+	case 1 :
+	  printf("yi");break;
+	case 2 :
+	  printf("er");break;
+	case 3 :
+	  printf("san");break;
+	case 4 :
+	  printf("si");break;
+	case 5 :
+	  printf("wu");break;
+	case 6 :
+	  printf("liu");break;
+	case 7 :
+	  printf("qi");break;
+	case 8 :
+	  printf("ba");break;
+	case 9 :
+	  printf("jiu");break;
+	default :
 	  break;
-	}
+      }
+     if(i!=0)
+      printf(" ");
     }
 }
